b9bdccd9d13957e8e7b88692cf25c7c1a07f66df
jperedo <peredo01@gmail.com>
2024-05-06 18:58:54 -0500

    describe image results

--- objectDetection.py
+++ objectDetection.py
@@ -1,13 +1,9 @@
 import io
 import matplotlib.pyplot as plt
-import requests
-# import inflect
 from PIL import Image
 from transformers import pipeline
-
-
-def load_image_from_url(url):
-    return Image.open(requests.get(url, stream=True).raw)
+from inflect import engine
+import sounddevice as sd
 
 
 def render_results_in_image(in_pil_img, in_results):
@@ -49,6 +45,37 @@ def render_results_in_image(in_pil_img, in_results):
 
     return modified_image
 
+
+def summarize_predictions_natural_language(predictions):
+    summary = {}
+    p = engine()
+
+    for prediction in predictions:
+        label = prediction['label']
+        if label in summary:
+            summary[label] += 1
+        else:
+            summary[label] = 1
+
+    result_string = "In this image, there are "
+    for i, (label, count) in enumerate(summary.items()):
+        count_string = p.number_to_words(count)
+        result_string += f"{count_string} {label}"
+        if count > 1:
+          result_string += "s"
+
+        result_string += " "
+
+        if i == len(summary) - 2:
+          result_string += "and "
+
+    # Remove the trailing comma and space
+    result_string = result_string.rstrip(', ') + "."
+
+    return result_string
+
+
+
 def detect_objects():
     dinner_image = Image.open("data/jason-briscoe-VBsG1VOgLIU-unsplash.jpg")
     # resized_img = dinner_image.resize((569, 491))
@@ -57,6 +84,12 @@ def detect_objects():
     # print(detection_result)
     processed_image = render_results_in_image(dinner_image, detection_result)
     processed_image.show()
+    return detection_result
 
 
-detect_objects()
+def run_app():
+    prediction = detect_objects()
+    text =  summarize_predictions_natural_language(prediction)
+    print(text)
+
+run_app()
