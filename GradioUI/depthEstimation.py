242b81e6d7e4d294288bf530bde288856f8f6197
jperedo <peredo01@gmail.com>
2024-05-07 16:34:44 -0500

    cleanup

--- GradioUI/depthEstimation.py
+++ GradioUI/depthEstimation.py
@@ -4,20 +4,16 @@ from PIL import Image
 import torch
 import numpy as np
 
+pipe = pipeline(task="depth-estimation", model="Intel/dpt-hybrid-midas")
+
 
 def depth_estimation(source_image: Image) -> Image:
-    pipe = pipeline(task="depth-estimation", model="Intel/dpt-hybrid-midas")
+
     output = pipe(source_image)
-    print(output)
-    print(output["predicted_depth"])
-    print(output["predicted_depth"].unsqueeze(1).shape)
     prediction = torch.nn.functional.interpolate(output["predicted_depth"].unsqueeze(1),
                                                  size=source_image.size[::-1],
                                                  mode="bicubic",
                                                  align_corners=False)
-    print(prediction.shape)
-    print(source_image.size[::-1])
-    print(prediction)
 
     # We need to convert the tensor to an image by normalizing it and converting it to uint8
     prediction_output = prediction.squeeze().numpy()
